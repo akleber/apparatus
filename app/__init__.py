61beef5e4a73ef25b98e67c2a412776cbd81cff6
Andreas Kleber <andreas@drosselweg7a.de>
2022-06-09 22:18:18 +0200

    add qr code

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,9 @@
 from flask import Flask
+from flask_qrcode import QRcode
 
 app = Flask(__name__)
+
+# https://marcoagner.github.io/Flask-QRcode/
+qrcode = QRcode(app)
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,13 @@
-from app import app
+from app import app, qrcode
+from flask import request, send_file, url_for
 
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello, World!"
+
+@app.route("/qr/<tinylink>", methods=["GET"])
+def qr(tinylink):
+    url = url_for('qr', tinylink=tinylink)
+    return send_file(qrcode(request.url_root[:-1] + url, mode="raw"), mimetype="image/png")
