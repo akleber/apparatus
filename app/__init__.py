da1aacf0105d8edabde568e1035b06f99eee9a55
Andreas Kleber <andreas@drosselweg7a.de>
2022-06-10 21:13:15 +0200

    add first database access

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,25 @@
-from flask import Flask
+from flask import Flask, g
 from flask_qrcode import QRcode
+import sqlite3
+
+
+DATABASE = 'apparatus.db'
 
 app = Flask(__name__)
 
 # https://marcoagner.github.io/Flask-QRcode/
 qrcode = QRcode(app)
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from app import app, qrcode
+from app import app, qrcode, get_db
 from flask import request, send_file, url_for, render_template, redirect
+import markdown
 
 
 @app.route("/")
@@ -12,7 +13,12 @@ def index():
 def register(tinylink):
     title = "AG Angebot Förderverein CUS Schuljahr 2021/22"
     user = {"username": "Miguel"}
-    return render_template("register.html", title=title, user=user)
+
+    cur = get_db().execute('SELECT description FROM event WHERE eventID = 1')
+    rv = cur.fetchall()
+    cur.close()
+
+    return render_template("register.html", title=title, user=user, description=markdown.markdown(rv[0][0]))
 
 
 @app.route("/qr/<tinylink>", methods=["GET"])
