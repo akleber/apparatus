1923ccfef86173e4427d9f1b7560383e7bf70b67
Andreas Kleber <andreas@drosselweg7a.de>
2022-06-28 17:21:52 +0200

    fix for sqlite 3.34

--- app/utils.py
+++ app/utils.py
@@ -24,8 +24,12 @@ def add_user(firstName: str, familyName: str, mail: str) -> int:
         "mailVerificationToken": str(uuid.uuid4()),
         "gdprToken": str(uuid.uuid4()),
     }
+    
+    # Adding  "RETURNING userID" at the end of next statement would be optimal,
+    # but it is only supported since 3.35. Last versionon Debian 11 is 3.34.1.
+    # Omitting it is fine, as userID is the autoincrment integer primary key of this table
     sql = """INSERT INTO user (firstName, familyName, mail, mailVerificationToken, gdprToken) 
-             VALUES (:firstName, :familyName, :mail, :mailVerificationToken, :gdprToken) RETURNING userID;"""
+             VALUES (:firstName, :familyName, :mail, :mailVerificationToken, :gdprToken);"""
     cur = get_db().execute(sql, user_data)
     userID = cur.lastrowid
     return userID, user_data
