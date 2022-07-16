be62a853514e59d06a57f3dc9484056b1c4502f8
Andreas Kleber <andreas@drosselweg7a.de>
2022-07-16 19:19:08 +0200

    election form validation, save to db

--- app/election/election_routes.py
+++ app/election/election_routes.py
@@ -1,5 +1,12 @@
 from app.election import bp, get_db
-from flask import render_template, current_app, abort
+from flask import render_template, current_app, abort, request
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import InputRequired, Length
+
+
+class VoteForm(FlaskForm):
+    name = StringField("Name", validators=[InputRequired(), Length(max=50)])
 
 
 @bp.route("/")
@@ -8,7 +15,7 @@ def index():
 
 
 @bp.route("/<uuid:electionID>")
-def vote(electionID):
+def election(electionID):
     # TODO deadline
     cur = get_db().execute(
         "SELECT * FROM election WHERE electionID = ? ",
@@ -16,7 +23,7 @@ def vote(electionID):
     )
     rv = cur.fetchone()
     if not rv:
-        current_app.logger.error(f"vote: electionID unknown")
+        current_app.logger.error(f"election: electionID unknown")
         return abort(404)
     election_data = dict(rv)
 
@@ -48,9 +55,32 @@ def vote(electionID):
         for row2 in cur2:
             names.add(row2[0])
 
+    form = VoteForm()
+
     return render_template(
         "electionVote.html",
         election_data=election_data,
         election_options=election_options,
         names=names,
+        form=form,
     )
+
+
+@bp.route("/<uuid:electionID>/vote", methods=["POST"])
+def vote(electionID):
+    form = VoteForm()
+    if not form.validate_on_submit():
+        current_app.logger.error(f"vote: form validation failed")
+        abort(400)
+
+    votes = request.form.getlist("option")
+    print(votes)
+    for vote in votes:
+        vote_data = {"name": form.name.data, "optionID": vote}
+
+        sql = """INSERT INTO election_vote (optionID, name) 
+                VALUES (:optionID, :name);"""
+        get_db().execute(sql, vote_data)
+
+    get_db().commit()
+    return "voted"
